1f09fff9a2ad75b027984c78983e0f9f033ef114
Gerard de la Fuente <gerarddela@gmail.com>
2024-09-20 23:39:06 +0200

    create android

---
+++ android/app/src/main/java/com/example/tabs/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.tabs;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Nombres de las pestañas
+    private String[] nombres = new String[]{"MIS LUGARES","CONSTANT LAYOUT","MÁS VISTAS","CALCULADORA"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Pestañas
+        ViewPager2 viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MiPagerAdapter(this));
+        TabLayout tabs = findViewById(R.id.tabs);
+        new TabLayoutMediator(tabs, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position){
+                        tab.setText(nombres[position]);
+                    }
+                }
+        ).attach();
+
+    }
+
+    public class MiPagerAdapter extends FragmentStateAdapter {
+        public MiPagerAdapter(FragmentActivity activity){
+            super(activity);
+        }
+        @Override
+        public int getItemCount() {
+            return 4;
+        }
+        @Override @NonNull
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0: return new Tab1();
+                case 1: return new Tab2();
+                case 2: return new Tab3();
+                case 3: return new Tab4();
+            }
+            return null;
+        }
+    }
+
+
+
+}
